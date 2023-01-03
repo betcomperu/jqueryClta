d9d2b21f28744037874c13f23df2b5691dd4dd10
Betoficina <albetho@hotmail.com>
2023-01-02 19:58:18 -0500

    Avanzando...!

--- js/codigo.js
+++ js/codigo.js
@@ -1,15 +1,19 @@
 var x;
-x=$(document);
+x = $(document);
 x.ready(iniciar);
 
-function iniciar(){
+function iniciar() {
   var x;
-x=$("#hola");
-x.click(verde);
+  x = $("#hola");
+  x.click(verde);
 }
-function verde(){
+function verde() {
   var x;
-  x=$("#hola");
+  x = $("#hola");
   x.css("color","red");
+  x.css("background-color","#F2F2F2");
+  x.css("font-size","24px");
+  x.css("border", "4px solid #bbbbaa");
+  x.css("text-align", "center");
+ 
 }
-
